2e43e9c098205dac490be8e2198f953b398ccb48
RAM <149155432+iaifilmsbotz@users.noreply.github.com>
2024-05-10 20:28:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://auto-filter-bot-xzac.onrender.com/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
